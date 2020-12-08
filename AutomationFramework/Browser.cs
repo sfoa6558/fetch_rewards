013bd34d4fd744ea89a48565cb553836e6dd106a
Fola Akinyemi <FAkinyemi@paylocity.com>
2020-12-07 21:37:36 -0600

    removed some unwanted using statements

--- AutomationFramework/Browser.cs
+++ AutomationFramework/Browser.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.IE;
 using System.Configuration;
-using OpenQA.Selenium.Support.UI;
 using System.IO;
 
 namespace AutomationFramework
